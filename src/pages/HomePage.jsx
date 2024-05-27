c1e3991fd956464b032569e1c9cd6367569275df
Keane <keane@usernoMacBook-Pro.local>
2024-05-27 15:41:55 +0900

    question page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,15 +6,15 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-custom overflow-hidden">
-      <div className="bg-white p-8 rounded-lg shadow-md text-center font-sans">
+      <div className="bg-white p-8 rounded-lg shadow-md text-center font-sans mt-4">
         <h1 className="text-4xl font-heading font-bold mb-4 ">Discover Your Horse Betting Personality</h1>
-        <Link to={"/questions/1"}>
+        <Link to={"/questions/0"}>
           <button className="bg-button-color text-white font-bold px-6 py-2 rounded-lg hover:bg-button-color-hover transition-colors duration-300">
             Start Now!
           </button>
         </Link>
       </div>
-      <div className="bg-container-color mt-4 p-4 rounded-lg shadow-md text-center">
+      <div className="bg-container-color my-6 p-4 rounded-lg shadow-md text-center">
         <PersonalityPane />
       </div>
     </div>
